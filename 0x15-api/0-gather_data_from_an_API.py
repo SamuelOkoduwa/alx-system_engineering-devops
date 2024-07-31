ff2805bbf6362fcc5a321904027f5b97514bd0d5
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-07-31 13:31:58 -0100

    removed the last line

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,36 +3,20 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-    
-    employee_id = sys.argv[1]
-    try:
-        employee_id = int(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-    
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_url = f"{base_url}users/{employee_id}"
-    todos_url = f"{base_url}todos?userId={employee_id}"
-    
+    user_id = int(sys.argv[1])
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+
     user_response = requests.get(user_url)
     todos_response = requests.get(todos_url)
-    
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error fetching data from API.")
-        sys.exit(1)
-    
+
     user_data = user_response.json()
     todos_data = todos_response.json()
-    
-    employee_name = user_data.get("name")
+
+    employee_name = user_data.get('name')
     total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get("completed") is True]
-    
+    done_tasks = [task for task in todos_data if task.get('completed')]
+
     print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t {task.get('title')}")
-
