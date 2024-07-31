69c08617534f3fe191649c834f90cadfff75eb3e
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-07-31 14:03:32 -0100

    Modifed fields

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,50 +1,44 @@
 #!/usr/bin/python3
 """
-This script uses the JSONPlaceholder API to gather data for a given employee ID
+This script uses the JSONPlaceholder API to gather data for all employees
 and exports the data in JSON format.
 """
 
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 2:
-        print("Usage: ./2-export_to_JSON.py <employee_id>")
-        sys.exit(1)
+    # URL for users and todos
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    # Get the employee ID from the command line argument
-    user_id = int(sys.argv[1])
-
-    # URLs for the user and the user's TODO list
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-
-    # Make a GET request to fetch user details
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-
-    # Make a GET request to fetch user's TODO list
+    # Make GET requests to fetch users and todos
+    users_response = requests.get(users_url)
     todos_response = requests.get(todos_url)
+
+    users_data = users_response.json()
     todos_data = todos_response.json()
 
-    # Extract the username from the user data
-    username = user_data.get('username')
-
     # Prepare data for JSON export
-    data = {
-        str(user_id): {
-            username: [
-                {
-                    "task": task.get('title'),
-                    "completed": task.get('completed')
-                }
-                for task in todos_data
-            ]
-        }
-    }
+    data = {}
+    for user in users_data:
+        user_id = user.get('id')
+        username = user.get('username')
+        # Filter tasks by user ID and prepare the list of dictionaries
+        user_tasks = [
+            {
+                "task": task.get('title'),
+                "completed": task.get('completed')
+            }
+            for task in todos_data if task.get('userId') == user_id
+        ]
+        # Store the tasks list under the user ID
+        data[str(user_id)] = [{
+            "username": username,
+            "tasks": user_tasks
+        }]
 
     # Write JSON data to file
-    with open(f"{user_id}.json", 'w') as file:
-        json.dump(data, file)
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
